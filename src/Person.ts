05a73c04432faf848ffc8e6f353b6499da8d6537
airtonlopsan <lopsan@msn.com>
2020-09-04 16:55:10 -0300

    work is done!

--- src/Person.ts
+++ src/Person.ts
@@ -1,19 +1,5 @@
-export  abstract class Person{
-    protected id: number; 
-    public name: string;
-    protected email: string; 
-    protected birthData: string; 
-    
-    constructor(id: number, 
-        name: string, 
-        email: string, 
-        birthData: string, 
-
-        ) {
-        this.id = id;
-        this.name = name; 
-        this.email = email; 
-        this.birthData = birthData;
-       
-    }
+export interface Person{
+    id: number; 
+    name: string;
+    email: string;
 }
